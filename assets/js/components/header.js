46f59ad6e5cbfd48e3a2d0d034370d5f07c95b35
Gospodin Chavdarov <g.chavdarov92@gmail.com>
2025-08-19 13:44:32 +0300

    adding scrolly for mobile nav, adding mobile navs for every page

--- assets/js/components/header.js
+++ assets/js/components/header.js
@@ -2,60 +2,12 @@
 class HeaderComponent {
   constructor() {
     this.header = document.getElementById('header');
-    this.menu = document.getElementById('menu');
-    this.body = document.body;
     this.init();
   }
 
   init() {
-    this.setupMenu();
     this.setupScrollEffects();
     this.setupMobileNav();
-  }
-
-  setupMenu() {
-    if (!this.menu) return;
-
-    this.menu.wrapInner('<div class="inner"></div>');
-    this.menu._locked = false;
-
-    this.menu._lock = () => {
-      if (this.menu._locked) return false;
-      this.menu._locked = true;
-      window.setTimeout(() => {
-        this.menu._locked = false;
-      }, 350);
-      return true;
-    };
-
-    this.menu._show = () => {
-      if (this.menu._lock()) {
-        this.body.addClass('is-menu-visible');
-      }
-    };
-
-    this.menu._hide = () => {
-      if (this.menu._lock()) {
-        this.body.removeClass('is-menu-visible');
-      }
-    };
-
-    // Menu toggle
-    $('.menu-toggle').on('click', (e) => {
-      e.preventDefault();
-      this.menu._show();
-    });
-
-    // Close menu on click outside
-    $(document).on('click', (e) => {
-      if (e.target.closest('#menu') || e.target.closest('.menu-toggle')) return;
-      this.menu._hide();
-    });
-
-    // Close menu on escape key
-    $(document).on('keydown', (e) => {
-      if (e.keyCode === 27) this.menu._hide();
-    });
   }
 
   setupScrollEffects() {
